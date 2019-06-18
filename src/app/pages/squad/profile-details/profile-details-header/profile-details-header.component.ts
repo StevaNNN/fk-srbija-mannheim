e181d4278404e89160591c30d2dc552372f0b95d
StevaNNN <stewica@live.com>
2019-06-18 20:20:21 +0200

    finished profile details header animation smooth babe :D

--- src/app/pages/squad/profile-details/profile-details-header/profile-details-header.component.ts
+++ src/app/pages/squad/profile-details/profile-details-header/profile-details-header.component.ts
@@ -1,18 +1,19 @@
-import { Component, Input, OnInit } from '@angular/core';
+import {AfterViewInit, Component, Input, OnInit} from '@angular/core';
 
 @Component ({
   selector: 'app-profile-details-header',
   templateUrl: './profile-details-header.component.html',
   styleUrls: ['./profile-details-header.component.scss']
 })
-export class ProfileDetailsHeaderComponent implements OnInit {
+export class ProfileDetailsHeaderComponent implements AfterViewInit {
 
   @Input () confing: any;
 
-  constructor() {
-  }
+  state = 'none';
 
-  ngOnInit() {
+  ngAfterViewInit() {
+    setTimeout(() => {
+      this.state = 'showPhoto';
+    }, 500);
   }
-
 }
