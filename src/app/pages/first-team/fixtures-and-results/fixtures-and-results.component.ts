834bcb8a21a062ee99e15feee096353fd819cd53
StevaNNN <stewica@live.com>
2019-06-25 23:47:00 +0200

    all bunch of stuff

--- src/app/pages/first-team/fixtures-and-results/fixtures-and-results.component.ts
+++ src/app/pages/first-team/fixtures-and-results/fixtures-and-results.component.ts
@@ -7,9 +7,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FixturesAndResultsComponent implements OnInit {
 
+  public teamsScore: any = [
+    {
+      playedDate: '26.6 JUN',
+      teamLeftName: 'FC Barcelona',
+      teamLeftAvatar: '../../../../assets/img/test-badge.jpg',
+      teamLeftScore: 1,
+      teamRightScore: 2,
+      teamRightName: 'FC Mannheim',
+      teamRightAvatar: '../../../../assets/img/test-badge.jpg'
+    },
+    {
+      playedDate: '26.6 JUN',
+      teamLeftName: 'FC Barcelona',
+      teamLeftAvatar: '../../../../assets/img/test-badge.jpg',
+      teamLeftScore: 1,
+      teamRightScore: 2,
+      teamRightName: 'FC Mannheim',
+      teamRightAvatar: '../../../../assets/img/test-badge.jpg'
+    },
+    {
+      playedDate: '26.6 JUN',
+      teamLeftName: 'FC Barcelona',
+      teamLeftAvatar: '../../../../assets/img/test-badge.jpg',
+      teamLeftScore: 1,
+      teamRightScore: 2,
+      teamRightName: 'FC Mannheim',
+      teamRightAvatar: '../../../../assets/img/test-badge.jpg'
+    },
+    {
+      playedDate: '26.6 JUN',
+      teamLeftName: 'FC Barcelona',
+      teamLeftAvatar: '../../../../assets/img/test-badge.jpg',
+      teamLeftScore: 1,
+      teamRightScore: 2,
+      teamRightName: 'FC Mannheim',
+      teamRightAvatar: '../../../../assets/img/test-badge.jpg'
+    },
+    {
+      playedDate: '26.6 JUN',
+      teamLeftName: 'FC Barcelona',
+      teamLeftAvatar: '../../../../assets/img/test-badge.jpg',
+      teamLeftScore: 1,
+      teamRightScore: 2,
+      teamRightName: 'FC Mannheim',
+      teamRightAvatar: '../../../../assets/img/test-badge.jpg'
+    }
+  ];
+
   constructor() { }
 
   ngOnInit() {
   }
-
 }
