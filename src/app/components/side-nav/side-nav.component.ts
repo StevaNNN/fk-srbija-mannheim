31017e561bdc053f67dca00a89829d33476ce824
StevaNNN <stewica@live.com>
2019-07-01 00:13:27 +0200

    all kind of stuff

--- src/app/components/side-nav/side-nav.component.ts
+++ src/app/components/side-nav/side-nav.component.ts
@@ -17,6 +17,24 @@ export class SideNavComponent implements OnInit {
     this.dropdownOpen = !this.dropdownOpen;
   }
 
+  public closeSideMenuOnActive(event) {
+    const btn = event.target.classList.contains('active');
+    const navLink = document.querySelector('.fcm-side-menu');
+    const header = document.querySelector('.fcm-navbar');
+    const closeBtn = document.querySelector('.close-white-big-icon');
+    const dropDown = document.querySelector('.fcm-side-nav-dropdown-menu');
+    const dropDownIcon = document.querySelector('.keyboard-arrow-up-icon');
+    if (btn) {
+      navLink.classList.remove('show');
+      header.classList.remove('side-menu-open');
+      closeBtn.classList.remove('close-white-big-icon');
+      closeBtn.classList.add('menu-white-big-icon');
+      dropDown.classList.remove('show');
+      dropDownIcon.classList.remove('keyboard-arrow-up-icon');
+      dropDownIcon.classList.add('keyboard-arrow-down-icon');
+    }
+  }
+
   ngOnInit() {
   }
 
